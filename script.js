951fb0eb4d348b1f1a2dd47ceb9baa68a2981231
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-09 11:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } 
+	catch (error) {
+        display.value = 'Error';        
+	}
+}
